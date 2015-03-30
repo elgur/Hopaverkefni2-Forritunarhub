0ff87659e96c6372fe54aaa0c3471b877756583b
elgur <emilthorv@gmail.com>
2015-03-30 18:05:19 +0000

    check is check
    síða er farinn að senda server hvaða box er búið að haka við.

--- test_sida_emil/app/views.py
+++ test_sida_emil/app/views.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request, jsonify
 from app import app
 from csvreader import lesaskra
 # setja inn í class ?
@@ -8,10 +8,19 @@ lykl = []
 for x in sorted(lyklar):
 	lykl.append(x)
 lyklar =lykl
-## setja inn í class? 
-@app.route('/')
-@app.route('/index')
+## setja inn í class?
+stillingar = [ ]
+
+@app.route('/', methods = ['GET','POST'])
+@app.route('/index', methods = ['GET','POST'])
 def index():
 		return render_template('index.html',
 								stadi='staði',
 								lyklarnir =lyklar)
+@app.route('/saekja', methods=['GET','POST'])
+def saekja_box():
+	stillingar = request.form.getlist("still")
+	stodvar = request.form.getlist("stod") 
+	print(stillingar)
+	print(stodvar)
+	return index()
